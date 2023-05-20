c7f831376b3c178686294151e6fd921a908fed57
LeonardoGarciah <leonardoformulaum@hotmail.com>
2023-05-20 14:35:05 -0300

    chore: config eslint & prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1>Hello world</h1>
+    <div className="h-screen bg-zinc-950">
+      <h1 className="text-4xl font-bold text-zinc-50">Sua capsula do tempo</h1>
+    </div>
   )
 }
