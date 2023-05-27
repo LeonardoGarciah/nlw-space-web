045080a3f132eef8db50d85cb7aa9b956fa06799
LeonardoGarciah <leonardoformulaum@hotmail.com>
2023-05-27 14:53:19 -0300

    feat: created user signIn

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+const Copyright = () => (
+  <div className="text-sm leading-relaxed text-gray-200">
+    Feito com 💜 no NLW da{' '}
+    <a
+      target="_blank"
+      rel="noreferrer"
+      className="underline hover:text-gray-100"
+      href="#"
+    >
+      Rocketseat
+    </a>
+  </div>
+)
+
+export default Copyright
